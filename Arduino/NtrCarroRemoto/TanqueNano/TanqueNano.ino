ed30e677d2f4eb6c562452a4fb8019606c122f73
Juan B <55644440+JnBenites@users.noreply.github.com>
2024-12-15 16:15:46 -0500

    casi funcionando los servos

--- Arduino/NtrCarroRemoto/TanqueNano/TanqueNano.ino
+++ Arduino/NtrCarroRemoto/TanqueNano/TanqueNano.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 // Definición de pines del módulo Bluetooth
 #define BT_RX 2 // D2 en Arduino Nano
@@ -17,6 +18,20 @@ SoftwareSerial Bluetooth(BT_RX, BT_TX);
 #define STEP2_IN3 10 // D10 en Arduino Nano
 #define STEP2_IN4 11 // D11 en Arduino Nano
 
+// Pines para servomotores
+#define SERVO1_PIN 12 // D12 en Arduino Nano
+#define SERVO2_PIN A0 // A0 en Arduino Nano
+#define SERVO3_PIN A1 // A1 en Arduino Nano
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+// Variables para las posiciones de los servos
+int servo1Pos = 90;
+int servo2Pos = 90;
+int servo3Pos = 90;
+
 void setup() {
   // Configuración de pines para motores paso a paso
   pinMode(STEP1_IN1, OUTPUT);
@@ -28,6 +43,16 @@ void setup() {
   pinMode(STEP2_IN2, OUTPUT);
   pinMode(STEP2_IN3, OUTPUT);
   pinMode(STEP2_IN4, OUTPUT);
+
+  // Configuración de servomotores
+  servo1.attach(SERVO1_PIN);
+  servo2.attach(SERVO2_PIN);
+  servo3.attach(SERVO3_PIN);
+
+  // Posicionar los servos en la posición inicial
+  servo1.write(servo1Pos);
+  servo2.write(servo2Pos);
+  servo3.write(servo3Pos);
 
   // Configuración de comunicación serial
   Serial.begin(9600);
@@ -67,6 +92,31 @@ void loop() {
         break;
       case 'S': // Detener motores
         stopMotors();
+        break;
+      // Control de servos con botones para izquierda y derecha
+      case 'X': // Servo 1 mover a la derecha
+        servo1Pos = min(servo1Pos + 10, 180);
+        servo1.write(servo1Pos);
+        break;
+      case 'x': // Servo 1 mover a la izquierda
+        servo1Pos = max(servo1Pos - 10, 0);
+        servo1.write(servo1Pos);
+        break;
+      case 'Y': // Servo 2 mover a la derecha
+        servo2Pos = min(servo2Pos + 10, 180);
+        servo2.write(servo2Pos);
+        break;
+      case 'y': // Servo 2 mover a la izquierda
+        servo2Pos = max(servo2Pos - 10, 0);
+        servo2.write(servo2Pos);
+        break;
+      case 'Z': // Servo 3 mover a la derecha
+        servo3Pos = min(servo3Pos + 10, 180);
+        servo3.write(servo3Pos);
+        break;
+      case 'z': // Servo 3 mover a la izquierda
+        servo3Pos = max(servo3Pos - 10, 0);
+        servo3.write(servo3Pos);
         break;
       default:
         Serial.println("Comando no reconocido.");
